ebe15c7f13866819c22ca1c37912c2f30caf35e1
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-07-12 19:13:21 +0400

    Working With Classes

--- script.js
+++ script.js
@@ -4,7 +4,25 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnClosedModal = document.querySelector('.close-modal');
 const btnsOpenModals = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModals);
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnsOpenModals.length; i++)
-  console.log(btnsOpenModals[i].textContent);
+  btnsOpenModals[i].addEventListener('click', openModal);
+
+// btnClosedModal.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+
+btnClosedModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
